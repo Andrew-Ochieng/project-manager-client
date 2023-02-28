005c48bd7624993f05d77dd35d47acac951e9bb7
Code-X <andrewomosh44@gmail.com>
2023-02-28 18:49:48 +0300

    add clients section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,24 +1,22 @@
-import hero from "../assets/hero.jpg"
+// import hero from "../assets/hero.jpg"
 
 const Hero = () => {
     return ( 
         <>
-            <div className=''
+            <div className='bg-hero'
                 style={{
-                        backgroundImage: `url(https://images.unsplash.com/photo-1552581234-26160f608093?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHByb2plY3QlMjBtYW5hZ2VtZW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60)`, 
                         backgroundSize: `cover`,
-                        paddingTop: `200px`, 
-                        paddingBottom: `200px` 
+                        paddingTop: `100px`, 
+                        paddingBottom: `100px` 
                     }}
                 >
-                <div className="flex flex-col justify-center items-center lg:my-24 md:my-16 my-6 md:mx-32 mx-8">
-                    <h1 className="title ">
-                        CHOOSE TO USE ONE OF THE BEST PROJECT MANAGERS FOR STUDENTS
+                <div className="flex flex-col justify-center items-center lg:my-24 md:my-16 my-6 md:mx-64 mx-8">
+                    <h1 className="title uppercase">
+                        Project Management Made Easy! 
                     </h1>
-                    <h2 className="font-semibold md:text-lg text-gray-100 uppercase">
-                        Home -
-                        <span className="text-sky-500 mx-2">Learn More</span>
-                    </h2>
+                    <button className="btn">
+                        Get Started
+                    </button>
                 </div>
             </div>
         </>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
+import Clients from "../components/Clients";
+import ContentCard from "../components/ContentCard";
 import Hero from "../components/Hero";
 
 const Home = () => {
     return ( 
         <>
             <Hero />
+            <Clients />
+            <ContentCard />
         </>
      );
 }

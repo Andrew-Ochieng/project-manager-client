e7ff98065a4b0e2d3db96008a0468722ecbbd27d
Vincent <makokhavomondi@gmail.com>
2023-03-01 19:23:12 +0300

    Use table to display existing projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -17,14 +17,35 @@ const Projects = () => {
 
 
     return ( 
-        <div>
-            {projects.map((project) => (
-                <div key={project.id}>
-                    <h2>{project.name}</h2>
-                    <h3>{project.topic}</h3>
-                    <p>{project.details}</p>
+        <div className="grid place-items-center min-h-screen px-20">
+            <div className="flex flex-col">
+                <div className="flex justify-between my-5">
+                    <h1 className="font-bold">YOUR PROJECTS</h1>
+                    <button className="border-solid border border-blue py-2 px-5 rounded-md">Add New</button>
                 </div>
-            ))}
+                <table>
+                    {
+                        <tr className="min-w-max-content border-x-solid border border-sky">
+                            <th>Name</th>
+                            <th>Topic</th>
+                            <th>Details</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    }
+                    {
+                        projects.map(project => (
+                            <tr className="border-x-solid border border-sky">
+                                <td className="px-3" >{project.name}</td>
+                                <td className="px-3">{project.topic}</td>
+                                <td className="px-3 max-w-sm">{project.details}</td>
+                                <td className="px-5"><button>Edit</button></td>
+                                <td className="px-5"><button>Delete</button></td>
+                            </tr>
+                        ))
+                    }
+                </table>
+            </div>
         </div>
     );
 }

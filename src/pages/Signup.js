5a2a7079f77730a545c7d4a5682ca982d79eb237
Vincent <makokhavomondi@gmail.com>
2023-03-02 13:45:41 +0300

    Fix a bug with having used useEffect but not imported it

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState , useEffect} from "react";
 import { Link, useNavigate } from "react-router-dom"
 import signupImg from '../assets/signup.png'
 import { apiHost } from "../Variables";

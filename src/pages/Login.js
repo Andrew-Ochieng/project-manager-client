9465aa788576f47042cb0ac52284b5fe399f31ed
Vincent <makokhavomondi@gmail.com>
2023-03-02 13:59:23 +0300

    :bug: Import useEffect before using it

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useEffect } from "react";
 import { Link , useNavigate} from "react-router-dom";
 import loginImg from "../assets/login.png"
 import { apiHost } from "../Variables";
